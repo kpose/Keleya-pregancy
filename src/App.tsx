e9b3f14d8496f62c86bf25f59a84efce69245dbe
kpose <jude@flutterwavego.com>
2023-08-27 15:58:14 +0100

    use the app navigator as application entry point

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,14 @@
-import {StyleSheet, Text, View} from 'react-native';
 import React from 'react';
-import Screen from './components/screen/Screen';
-import {NavigationContainer} from '@react-navigation/native';
+import 'react-native-gesture-handler';
+import {SafeAreaProvider} from 'react-native-safe-area-context';
+import {AppNavigator} from './navigation/AppNavigator';
 
 const App = () => {
   return (
-    <NavigationContainer>
-      <Screen>
-        <View style={styles.container}>
-          <Text>App</Text>
-        </View>
-      </Screen>
-    </NavigationContainer>
+    <SafeAreaProvider>
+      <AppNavigator />
+    </SafeAreaProvider>
   );
 };
 
 export default App;
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
